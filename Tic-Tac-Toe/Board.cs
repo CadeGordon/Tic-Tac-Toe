0c9e6f94102cccfef74de931bbcd30354bf1b860
Cade Gordon <cadegordon093@gmail.com>
2021-10-04 14:41:02 -0500

    made board function

--- Tic-Tac-Toe/Board.cs
+++ Tic-Tac-Toe/Board.cs
@@ -9,6 +9,44 @@ namespace Tic_Tac_Toe
         private char _player1Token;
         private char _player2Token;
         private char _currentToken;
+        private char[,] _board;
+
+        public void Start()
+        {
+            _player1Token = 'x';
+            _player2Token = 'o';
+            _currentToken = _player1Token;
+            _board = new char[3, 3];
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void End()
+        {
+
+        }
+
+        /// <summary>
+        /// Assigns the spot at the given indices in the board array
+        /// </summary>
+        /// <param name="token">The token to set the array index to.</param>
+        /// <param name="posX">The x position of the token.</param>
+        /// <param name="posY">The y postion of the token.</param>
+        /// <returns>Retunr false if the indices are out of range</returns>
+        public bool SetToken(char token, int posX, int posY)
+        {
+            return false;
+        }
+
+        private bool CheckWinner(char token)
+        {
+
+        }
 
     }
+    
+    
 }
--- Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Program.cs
@@ -4,43 +4,12 @@ namespace Tic_Tac_Toe
 {
     class Program
     {
+        //test
         static void Main(string[] args)
         {
-            int[,] array2DInitExample = new int[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
 
-
-            int[,] array2DInitExample2 = { { 1, 2, 3 }, 
-                                           { 4, 5, 6 },
-                                           { 7, 8, 9 },
-                                           { 10, 11, 12 } };
-
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    Console.WriteLine(array2DInitExample[i, j]);
-                }
-            }
-
-            void PrintSumOf2DArray(int [,] array)
-            {
-
-                for (int i = 0; i < array.GetLength(0); i++)
-                {
-                    int sum = 0;
-
-                    for (int j = 0; j < array.GetLength(1); j++)
-                    {
-                        sum += array[i, j];
-                    }
-                    Console.WriteLine(sum);
-                }
-
-                
-            }
-
-            PrintSumOf2DArray(array2DInitExample);
-            
         }
+
+            
     }
 }
